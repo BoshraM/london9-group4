f24c903610f904ba6bb0b32fc48fac258c0101c5
MohammadSefidgar <108987757+MohammadSefidgar@users.noreply.github.com>
2023-07-07 00:39:09 +0100

    Update

--- client/src/component/CodeWarsCard.js
+++ client/src/component/CodeWarsCard.js
@@ -3,10 +3,11 @@ import React from "react";
 const CodeWarsCard = ({ rank, progress, milestone }) => {
   return (
     <div className="codewars-card">
-      <h2>CodeWars</h2>
+      <h2>CodeWars Milestone</h2>
       <p>Your Rank: {rank}</p>
+      <p>You are on the track</p>
       <p>Progress: {progress}%</p>
-      <p>Next Milestone: {milestone}</p>
+      <p>You need to catch: {milestone}</p>
     </div>
   );
 };
